32e846abdb03d4369345c6d912a2582aa5a68d59
Chris <nguyen.chriss@gmail.com>
2016-05-20 20:41:55 -0500

    (feat)Added Routes, navbar, header & footer

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,1 +1,11 @@
-var app = angular.module("ssApp",[]);
+var app = angular.module("ssApp",['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/',{template:'This is the default Route'})
+    .when('/services',{template:'This is the services Route'})
+    .when('/about',{template:'This is the about Route'})
+    .when('/faq',{template:'This is the faq Route'})
+    .when('/contact',{template:'This is the contact Route'})
+    .otherwise({redirectTo:'/'});
+}]);
